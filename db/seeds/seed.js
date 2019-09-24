713a1902a0ea40d46ce2261812403b6315cd9393
Patrick Mackintosh <patrickgmackintosh@gmail.com>
2019-09-24 16:57:04 +0100

    Seeding in progress - topicData, articleData and userData inserted, just commentData to complete

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -7,33 +7,26 @@ const {
 
 const { formatDates, formatComments, makeRefObj } = require('../utils/utils');
 
-exports.seed = function(knex) {
-  const topicsInsertions = knex('topics').insert(topicData);
-  const usersInsertions = knex('users').insert(userData);
-
-  return Promise.all([topicsInsertions, usersInsertions])
+exports.seed = function(connection) {
+  return connection.migrate
+    .rollback()
+    .then(() => connection.migrate.latest())
     .then(() => {
-      /* 
-      
-      Your article data is currently in the incorrect format and will violate your SQL schema. 
-      
-      You will need to write and test the provided formatDate utility function to be able insert your article data.
-
-      Your comment insertions will depend on information from the seeded articles, so make sure to return the data after it's been seeded.
-      */
+      const topicInsertion = connection
+        .insert(topicData)
+        .into('topics')
+        .returning('*');
+      const userInsertion = connection
+        .insert(userData)
+        .into('users')
+        .returning('*');
+      return Promise.all([topicInsertion, userInsertion]);
     })
-    .then(articleRows => {
-      /* 
-
-      Your comment data is currently in the incorrect format and will violate your SQL schema. 
-
-      Keys need renaming, values need changing, and most annoyingly, your comments currently only refer to the title of the article they belong to, not the id. 
-      
-      You will need to write and test the provided makeRefObj and formatComments utility functions to be able insert your comment data.
-      */
-
-      const articleRef = makeRefObj(articleRows);
-      const formattedComments = formatComments(commentData, articleRef);
-      return knex('comments').insert(formattedComments);
+    .then(() => {
+      const formatArticleData = formatDates(articleData);
+      return connection
+        .insert(formatArticleData)
+        .into('articles')
+        .returning('*');
     });
 };
